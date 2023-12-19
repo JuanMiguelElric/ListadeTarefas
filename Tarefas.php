54dac112fcdf60e727673a6b66e577f6a1aca804
Juan Miguel <oliveirajuan243@gmail.com>
2023-12-19 14:40:24 -0300

    Sistema de Tarefas completo

--- Tarefas.php
+++ Tarefas.php
@@ -1,112 +1,32 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciador de Tarefas</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
+<?php
+session_start();
 
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            margin-top: 20px;
-        }
+if (isset($_GET['nome']) && $_GET['nome'] != '') {
+    $tarefa = array();
+    $tarefa['nome'] = $_GET['nome'];
 
-        h1 {
-            color: #333;
-        }
+    if (isset($_GET['descricao'])) {
+        $tarefa['descricao'] = $_GET['descricao'];
+    } else {
+        $tarefa['descricao'] = '';
+    }
 
-        form {
-            margin-bottom: 20px;
-        }
+    if (isset($_GET['prazo'])) {
+        $tarefa['prazo'] = $_GET['prazo'];
+    } else {
+        $tarefa['prazo'] = '';
+    }
 
-        fieldset {
-            border: 1px solid #ddd;
-            padding: 10px;
-            border-radius: 5px;
-        }
+    $tarefa['prioridade'] = $_GET['prioridade'];
 
-        label {
-            display: block;
-            margin-bottom: 10px;
-        }
+    if (isset($_GET['concluida'])) {
+        $tarefa['concluida'] = $_GET['concluida'];
+    } else {
+        $tarefa['concluida'] = '';
+    }
 
-        input[type="text"] {
-            width: 100%;
-            padding: 8px;
-            box-sizing: border-box;
-            margin-bottom: 10px;
-        }
+    $_SESSION['listaTarefas'][] = $tarefa;
+}
 
-        input[type="submit"] {
-            background-color: #4caf50;
-            color: #fff;
-            border: none;
-            padding: 10px 15px;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-
-        table {
-            width: 100%;
-            margin-top: 20px;
-            border-collapse: collapse;
-        }
-
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #4caf50;
-            color: #fff;
-        }
-
-    </style>
-</head>
-<body>
-    <h1>Gerenciador de Tarefas</h1>
-    <form action="" method="get"> <!-- Adicionei o método "get" para enviar os dados via URL -->
-        <fieldset>
-            <legend>Nova Tarefa</legend>
-            <label for="nome">
-                <input type="text" name="nome" id="nome"> <!-- Adicionei um id ao input -->
-            </label>
-            <input type="submit" value="Cadastrar">
-        </fieldset>
-    </form>
-    <?php
-        // $_GET é usado para armazenar os dados que estão na URL
-        if(isset($_GET['nome'])){
-            $_SESSION['listadeTarefas'][] = $_GET['nome']; // Corrigi o nome da variável
-        }
-        $listadeTarefas = array();
-        if(isset($_SESSION['listadeTarefas'])){
-            $listadeTarefas = $_SESSION['listadeTarefas'];
-        }
-    ?>
-    <table>
-        <tr>
-            <th>Tarefas</th>
-        </tr>
-        <?php foreach($listadeTarefas as $tarefa): ?>
-            <tr>
-                <td><?php echo $tarefa; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
-</html>
+$listaTarefas = isset($_SESSION['listaTarefas']) ? $_SESSION['listaTarefas'] : array();
+?>
---
+++ template.php
@@ -0,0 +1,124 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            color: #333;
+            text-align: center;
+        }
+
+        form {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        fieldset {
+            border: none;
+            margin-bottom: 20px;
+        }
+
+        legend {
+            font-weight: bold;
+            color: #333;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        input[type="text"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            margin-top: 5px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #4caf50;
+            color: #fff;
+        }
+    </style>
+</head>
+<body> 
+    <?php include 'Tarefas.php'?>
+    <h1>Gerenciador de Tarefas</h1>
+    <form action="" method="get">
+        <fieldset>
+            <legend>Nova Tarefa</legend>
+            <label for="">
+                <input type="text" name="nome" id="">
+            </label>
+            <label for="">
+                Descrição
+                <textarea name="descricao" id="" cols="30" rows="10"></textarea>
+            </label>
+            <label for="">
+                prazo
+                <input type="text" name="prazo" id="">
+            </label>
+            <fieldset>
+                <legend>Prioridade:</legend>
+                <label for="">
+                    <input type="radio" name="prioridade " value="baixa" checked id=""> Baixa
+                    <input type="radio" name="prioridade " value="baixa" id=""> Media
+                    <input type="radio" name="prioridade " value="baixa" id=""> Alta
+                </label>
+            </fieldset>
+            <input type="submit" value="cadastrar">
+        </fieldset>
+    </form>
+    <table>
+        <tr>
+            <th>Tarefas</th>
+        </tr>
+        <?php foreach($listaTarefas as $tarefa):?>
+            <tr>
+                <td><?php echo $tarefa['nome'];?></td>
+                <td><?php echo $tarefa['descricao'];?></td>
+                <td><?php echo $tarefa['prazo'];?></td>
+                <td><?php echo $tarefa['prioridade'];?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
